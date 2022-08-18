581fa5e4921122c684a4334521740d191f2e6d25
kki4504 <rhkddlf9876@gmail.com>
2022-08-18 09:22:51 +0900

    json-server

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+        <div className='border-2 border-solid border-blue-200'>
+          hhhhhh
+        </div>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
